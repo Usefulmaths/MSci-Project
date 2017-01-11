9fc57253ce1e89511b120d0ffcac2d45624ccede
Liam <kingliam28@ntlworld.com>
2017-01-11 00:30:40 +0000

    Some changes

--- fidelity_functions.py
+++ fidelity_functions.py
@@ -7,7 +7,7 @@ from math import cos
 
 j = complex(0, 1)
 G = quantum_half_adder()
-gate_dimension = G.shape[0]
+gate_dimension = float(G.shape[0])
 
 # Dimension of states we care about (Region qubits)
 dimension_state_care = int(log(gate_dimension, 2))
@@ -21,48 +21,53 @@ def generate_dont_care_states(N, dimension_state_care):
     return tensor(states_dont_care), tensor(identity_dont_care)
 
 # Extracts non zero element positions of the gate
-def getGate(G): 
+def getGate (G): #extract non zero elements of the gate and save them in s
     
-    non_zero_elements = []
+    s = []
 
     rows = G.shape[0]
     colums = G.shape[1]
     
     for i in range(rows):
         for j in range(colums):
-            if G[i][0][j] != 0: 
-                non_zero_elements.append([i,j])
-    return non_zero_elements
+            if G[i][0][j] != 0 :
+                s.append([i,j])
+    return s
 
-# Converts a decimal number into its binary representation (computation basis).    
-def get_bin(decimal_number): 
-    binary_rep = bin(decimal_number)[2:]
-    length_binary_rep = len(binary_rep)
-    if length_binary_rep < 4: 
-        append_zeros = '0' * (4 - length_binary_rep)
-        binary_rep = append_zeros + binary_rep
-        
-    return binary_rep
-
-# Converts decimal numbers into quantum basis states.
-def getBasis(decimal_number):  
-    if decimal_number == 0:
-        B = [basis(2, 0)]*(dimension_state_care)
-        return tensor(B)
+def get_bin(a):  #binary representation of a number a: useful to write the computational basis 
     
-    binary_rep = get_bin(decimal_number)
+    s = bin(a)[2:]
+    l = len(s)
+    if l < 4 :
+        r = '0'*(4-l)
+        s = r + s
+        
+    return s
+
+def getBasis (a) : #get the basis states according to the binary of a: 10 -> |10>
+    
+    if a == 0:
+          B = [basis(2,0)]*(dimension_state_care)
+          return tensor(B)
+    
+    c = get_bin(a)
+    if gate_dimension == 16:
+        return tensor(basis(2,int(c[0])) , basis(2,int(c[1])), basis(2,int(c[2])), basis(2,int(c[3])))
     if gate_dimension == 8:
-        return tensor(basis(2, int(binary_rep[0])), basis(2, int(binary_rep[1])), basis(2, int(binary_rep[2])))
+        return tensor(basis(2,int(c[0])) , basis(2,int(c[1])), basis(2,int(c[2])))
+    if gate_dimension == 4:
+        return tensor(basis(2,int(c[1])) , basis(2,int(c[2])))
 
 states_dont_care, identity_dont_care = generate_dont_care_states(N, dimension_state_care)
 
 # Calculates the average fidelity.
 def Fidelity (J):  
-    gate_element_positions = getGate(G)
     H = hamiltonian(J)
-    Fid = 1/(gate_dimension + 1)
     U = (-j*H).expm()
     Udag = U.dag()
+
+    gate_element_positions = getGate(G)
+    Fid = 1.0/(gate_dimension + 1)
 
     
     for x in gate_element_positions: 
@@ -78,7 +83,28 @@ def Fidelity (J):
             Gstar_ik = G[x[0],x[1]].conjugate()
             G_jl = G[y[0],y[1]]
             
-            fidStep = (1/(gate_dimension*(gate_dimension+1)))*Gstar_ik*Eps_ijkl*G_jl     
-            Fid += abs(fidStep[0][0][0])
+            fidStep = (1.0/(gate_dimension*(gate_dimension+1)))*Gstar_ik*Eps_ijkl*G_jl     
+            Fid += fidStep[0][0][0]
             
-    return -Fid
+    return -abs(Fid)
+
+def average_fidelity(J):
+    H = hamiltonian(J)
+    U = (-j * H).expm()
+    Udag = U.dag()
+
+    total_fid = 0
+
+    for iters in range(10):
+        psi0 = tensor(rand_ket(N = 2), rand_ket(N = 2), rand_ket(N = 2))
+        psi_state = tensor(psi0, rand_ket(N = 2))
+    
+        epsilon = (U * ket2dm(psi_state) * U.dag()).ptrace([0, 1, 2])
+
+        fid_contribution = psi0.dag() * G.dag() * epsilon * G * psi0
+        total_fid += fid_contribution
+    
+    return total_fid / 10
+        
+
+
