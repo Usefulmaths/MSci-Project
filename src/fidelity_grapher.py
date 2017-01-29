65a349c0d7ca14c369861ff1aee7c668ffe1a069
Liam <kingliam28@ntlworld.com>
2017-01-29 22:46:59 +0000

    Add fidelity grapher.

---
+++ src/fidelity_grapher.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+while True:
+
+	data = pd.read_csv('toffoli_tests.txt', names=["index", "value"])
+
+	X = np.array(data.index)
+	y = np.array(data.value)
+
+	y = pd.rolling_mean(data.value, window = 50)
+
+	plt.ion()
+	plt.xlabel('Number of iterations')
+	plt.ylabel('Likelihood Fidelity')
+	plt.title('SGD likelihood for QHA')
+	plt.plot(X, y, color='r')
+	
+	plt.pause(2)
--- src/network_functions.py
+++
@@ -1,5 +0,0 @@
-from qutip import *
-
-## Definition of the quantum half-adder.
-def quantum_half_adder():
-	return tensor(cnot(), qeye(2)) * toffoli()
